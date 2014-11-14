f37f61271c71703eef507efb0151247c8380eaa8
Chris Pinkham <cpinkham@bc2va.org>
2014-11-13 23:28:23 -0800

    Remove old minimal help page under the Help menu.

--- help.php
+++
@@ -1,8 +0,0 @@
-<div class='fppTabs'>
-	<div class='title'>Matrix Tools Help</div>
-
-	The Matrix Tools Plugin can be used to place dynamic text or images
-	on a Matrix or Pixel Tree.  It also allows drawing on the display
-	item using a mouse or touch device.
-</div>
-
