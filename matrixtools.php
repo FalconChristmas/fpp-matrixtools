892fc3db8d4072dd5d5dbac6b2965608cee33ad4
Chris Pinkham <cpinkham@bc2va.org>
2015-11-20 23:34:51 -0800

    Add a bunch more scroll speeds to account for high-density LED panels.

--- matrixtools.php
+++ matrixtools.php
@@ -552,12 +552,12 @@ var quarterCellSize = Math.floor(halfCellSize / 2);
 							<option value='2'>2</option>
 							<option value='3'>3</option>
 							<option value='4'>4</option>
-							<option value='5' selected>5</option>
+							<option value='5'>5</option>
 							<option value='6'>6</option>
 							<option value='7'>7</option>
 							<option value='8'>8</option>
 							<option value='9'>9</option>
-							<option value='10'>10</option>
+							<option value='10' selected>10</option>
 							<option value='11'>11</option>
 							<option value='12'>12</option>
 							<option value='13'>13</option>
@@ -568,6 +568,22 @@ var quarterCellSize = Math.floor(halfCellSize / 2);
 							<option value='18'>18</option>
 							<option value='19'>19</option>
 							<option value='20'>20</option>
+							<option value='25'>25</option>
+							<option value='30'>30</option>
+							<option value='35'>35</option>
+							<option value='40'>40</option>
+							<option value='45'>45</option>
+							<option value='50'>50</option>
+							<option value='55'>55</option>
+							<option value='60'>60</option>
+							<option value='65'>65</option>
+							<option value='70'>70</option>
+							<option value='75'>75</option>
+							<option value='80'>80</option>
+							<option value='85'>85</option>
+							<option value='90'>90</option>
+							<option value='95'>95</option>
+							<option value='100'>100</option>
 							</select>
 							</td>
 						</tr>
